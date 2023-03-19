d12df6d11df74f62f77d15eb203a66b3f5737a21
Bukky2real <irekannenna@gmail.com>
2023-03-19 13:08:28 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main function to print with proper grammer
+ *
+ * Return: always 0 when correct
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
