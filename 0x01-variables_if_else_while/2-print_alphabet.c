6f5b5a607374d5d0513c2441a7013aec24a7aeb2
Bukky2real <irekannenna@gmail.com>
2023-03-20 20:02:58 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Reture: at all time 0 when successful
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+		       putchar(c);
+	       c++;
+		}
+	putchar('\n');
+	return (0);
+}
