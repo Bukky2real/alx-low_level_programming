ccd5216e6097a18f30f984288c238d8a116b5df2
Bukky2real <irekannenna@gmail.com>
2023-03-20 21:34:34 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main Entry
+ *
+ * Return: always 0 when successful
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+while
+	(c <= 'z')
+		if ((c != 'q' && c != 'e') && c <= 'z')
+			putchar(c);
+				c++;
+	putchar('\n');
+	return (0);
+}
